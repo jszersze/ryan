d0c1329abcdafb46fc68e385de6a1332c925b17c
Kuba <kpx.dev@gmail.com>
2021-10-01 14:59:39 -0700

    update

--- api/services/questions.js
+++ api/services/questions.js
@@ -5,6 +5,8 @@ module.exports = function (app) {
   app.post('/question', async (req, res) => {
     const reply = await process.accept(req.query);
 
+    console.log('POST', req);
+
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(message.reply(reply));
   });
@@ -18,6 +20,8 @@ module.exports = function (app) {
 
   app.get('/question', async (req, res) => {
     const reply = await process.accept(req.query);
+
+    console.log('GET', req);
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(message.reply(reply));
